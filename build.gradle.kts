e8ec210c64cbea64f5eac85aebbba88ddb47d2a9
Victor Miranda <victormiranda@valhalla-labs.es>
2024-02-02 22:01:03 +0100

    #6 Add Jackson-Kotlin module dependency
    Added Jackson Kotlin processing module as a dependency in "gradle/libs.versions.toml" and "build.gradle.kts". This module will offer better support for Kotlin-specific handling such as data classes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ subprojects {
     apply(plugin = "kotlin")
 
     dependencies {
+        implementation(rootProject.libs.jackson.kotlin)
         testImplementation(rootProject.testLibs.kotlin.test.junit5)
         testImplementation(rootProject.testLibs.junit.jupiter.api)
         testImplementation(rootProject.testLibs.junit.jupiter.engine)
